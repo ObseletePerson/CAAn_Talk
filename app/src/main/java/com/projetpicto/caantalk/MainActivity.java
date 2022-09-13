93c560d8077a35c200ef67104e1e7e1c948ee4d0
IsabelleRioux <samoyede2003@gmail.com>
2022-09-13 17:06:38 -0400

    création d'un bouton disant banane

--- app/src/main/java/com/projetpicto/caantalk/MainActivity.java
+++ app/src/main/java/com/projetpicto/caantalk/MainActivity.java
@@ -3,13 +3,46 @@ package com.projetpicto.caantalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextToSpeech lecture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ajouter splash screen
+
+        lecture = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i != TextToSpeech.ERROR){
+                    lecture.setLanguage(Locale.CANADA_FRENCH);
+                }
+            }
+        });
+
+        ImageButton banane = (ImageButton) findViewById(R.id.banane);
+
+        banane.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        lecture.speak(getString(R.string.banane), TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void onPause(){
+        if(lecture !=null){
+            lecture.stop();
+            lecture.shutdown();
+        }
+        super.onPause();
+    }
+
 }
